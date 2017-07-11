47242175508f5dbcd22c6fce4a491d0be19af25d
Marwa Mahmoud <Marwa@Marwa--Mariam-MacBook-Pro.local>
2017-07-11 00:57:14 -0700

    displaying the health power of each character

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # RPG-Game
+
+- The game starts by displaying four images of four star war characters
+- Each image has the name and the power of its character
+- When the user clicks on one images it becomes "your character" and the rest of the images become "enemies available to attack" they move to the ememies section and they are framed in red.
+- When the user clicks on one of the available enemies it becomes the defender and it moves to the defender section and it is framed in black
+- When the user clicks on the "Attack" button, a text is displayed showing the attack damage of the user vs that of the enemy;
--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,20 +1,32 @@
 
 $(document).ready(function(){
 
+	var myHealth = 0;
+	var enemyHealth = 0;
 
 	$("#original-images").children().attr("data-type", "original");
+	$("#first-character").children("p").text($("#first-character").attr("health-power"));
+	$("#second-character").children("p").text($("#second-character").attr("health-power"));
+	$("#third-character").children("p").text($("#third-character").attr("health-power"));
+	$("#fourth-character").children("p").text($("#fourth-character").attr("health-power"));
+
 
 
 	$("[data-type=original]").on("click", function(){
 
 		$("#selected-character").append($(this));
+		myHealth = $(this).attr("health-power");
+		console.log(myHealth);
 		$("#enemies").append($("#original-images").children());
 		$("#enemies").children().css("border", "solid 10px red");
 		$("#enemies").children().attr("data-type", "enemy");
 
+
 		$("[data-type=enemy]").on("click", function(){
 
 			$("#defender").append($(this));
+			enemyHealth = $(this).attr("health-power");
+			console.log(enemyHealth);
 			$(this).css("border", "solid 10px black");
 		});
 
