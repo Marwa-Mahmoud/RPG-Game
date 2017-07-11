0f39610ae12ee19b3f083d6cc43dd0f7e83ffcf9
Marwa Mahmoud <Marwa@Marwa--Mariam-MacBook-Pro.local>
2017-07-11 15:38:01 -0700

    Working game

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,7 +1,7 @@
 
 $(document).ready(function(){
 
-	
+	// creating golbal variables
 	var myHealth = "";
 	var enemyHealth = "";
 	var myDamage = "";
@@ -11,7 +11,9 @@ $(document).ready(function(){
 	var me;
 	var enemy;
 	var restartBtn = $("<button id='restart-btn'>Restart</button>");
+	noEnemyFlag = true; //it flags if there is no enemy to defend
 
+	//displaying data on each of the images
 	$("#original-images").children().attr("data-type", "original");
 	$("#obi-wan-kanobi").children("p").text($("#obi-wan-kanobi").attr("health-points"));
 	$("#luke-skywalker").children("p").text($("#luke-skywalker").attr("health-points"));
@@ -35,9 +37,12 @@ $(document).ready(function(){
 			enemyName = enemy.attr("name");
 			enemy.css({"border" : "solid 5px green", "color": "white", "background-color": "black"});
 			$("#attack-btn").attr("disabled", false);
+			$("#result").html("");
+			noEnemyFlag = false;
 		});
 	}	
 
+	// The first on click function that selects the user's player and sets the available enemies
 	$("[data-type=original]").on("click", function(){
 
 		me = $(this);
@@ -58,41 +63,56 @@ $(document).ready(function(){
 
 	});
 
-
-	//button is clickable only after the enemy is chosen???
-	//buttons stops responding to clicking when game is over
-
+	// Attack button 
 	$("#attack-btn").on("click", function(){
 
-		$("#result").html("You attacked "+ enemyName + " for "+ myAttackPower+ "<br>"+
-			enemyName + " attacked you back for " + enemyDamage);
+		if(noEnemyFlag){
+			$("#result").html("No enemy here!");
+
+		}
+
+		else{
+			$("#result").html("You attacked "+ enemyName + " for "+ myAttackPower+ "<br>"+
+				enemyName + " attacked you back for " + enemyDamage);
+
+	
+			enemyHealth-=myAttackPower;
+
+			if (enemyHealth <= 0){
+				console.log($("#enemies").children().length);
+
+				if($("#enemies").children().length > 1){
+
+					$("#result").html("You have defeated "+ enemyName+", you can choose to fight another enemy."+"<br>");
+					
+					noEnemyFlag = true;
+				}
+				else{
+					$("#result").html("YOU WON!!!...GAME OVER!!!");
+				}
+				enemy.remove();
+
+			}
+
+			else{
+				myHealth-= enemyDamage;
+
+				if(myHealth <= 0){
+					console.log("you lose");
+					$("#result").html("You have been defeated...GAME OVER!!!"+"<br>");
+					// Attack button stops responding to clicks when game is over
+					$("#attack-btn").attr("disabled", true);
+					$("#result").append(restartBtn);
+				}
+			}	
+
+			myAttackPower+= myDamage;
+			me.children("p").text(myHealth);
+			enemy.children("p").text(enemyHealth);
+
+		}
 
 		
-		myHealth-= enemyDamage;
-		enemyHealth-=myDamage;
-		myAttackPower+= myDamage;
-		me.children("p").text(myHealth);
-		enemy.children("p").text(enemyHealth);
-
-		console.log(myHealth+", "+ enemyHealth+", "+ myDamage);
-
-
-		if(myHealth <= 0){
-			console.log("you lose");
-			$("#result").html("You have been defeated...GAME OVER!!!"+"<br>");
-			$("#attack-btn").attr("disabled", true);
-			$("#result").append(restartBtn);
-	
-		}
-
-		else if (enemyHealth <= 0){
-			console.log("you win");
-
-			$("#result").html("You have defeated"+ enemyName+", you can choose to fight another enemy."+"<br>");
-			enemy.remove();
-			$("#attack-btn").attr("disabled", true);
-
-		}
 
 	});
 
